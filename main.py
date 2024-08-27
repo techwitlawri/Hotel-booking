5ca73694e0e00a9f861ff497b13201c5c183f8c4
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-27 08:59:15 +0100

    implement the methods of the reservation ticket

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ df = pandas.read_csv("hotels.csv", dtype= {"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name= df.loc[df["id"]== self.hotel_id, "name"].squeeze()
  
     def book(self):
         """Book a hotel by chaning its availability to no"""
@@ -26,11 +27,16 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self,customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
     def generate(self):
-        # content = f"Name of the customer hotel"
-        # return content
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name:{self.customer_name}
+        Hotel name: {self.hotel.name}"""
+        return content
         
 
 print(df) 
@@ -40,7 +46,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book() 
     name = input("Enter your name: ")
-    reservation_ticket= ReservationTicket(name, hotel)
+    reservation_ticket= ReservationTicket(customer_name= name, hotel_object= hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not free.")
